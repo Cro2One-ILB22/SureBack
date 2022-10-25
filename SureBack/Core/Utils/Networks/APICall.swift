af0cc0ca5f5f1c1ce6a60ce89c9374a0d3808b9e
Fahmi Al <mfahmiak@gmail.com>
2022-10-25 15:11:47 +0700

    Base url with env var

---
+++ SureBack/Core/Utils/Networks/APICall.swift
@@ -0,0 +1,27 @@
+//
+//  APICall.swift
+//  SureBack
+//
+//  Created by Muhamad Fahmi Al Kautsar on 25/10/22.
+//
+
+import Foundation
+
+struct API {
+    static let baseUrl: String = ProcessInfo.processInfo.environment["BASE_URL"] ?? "http://localhost:8000"
+}
+
+protocol Endpoint {
+    var url: String { get }
+}
+
+enum Endpoints: Endpoint {
+    case login
+
+    public var url: String {
+        switch self {
+        case .login:
+            return "\(API.baseUrl)/api/auth/login"
+        }
+    }
+}

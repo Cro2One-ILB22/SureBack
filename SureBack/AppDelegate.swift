3bb33dd61ad7d0177fad1f2a8c5684b4d4e61dc1
dithanrchy <dithanurcahya55@gmail.com>
2022-10-27 00:13:18 +0800

    deleting main storyboard

--- SureBack/AppDelegate.swift
+++ SureBack/AppDelegate.swift
@@ -9,8 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let nav = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
         return true
     }
 }

802fbac7f871124c4709b9d0b606060a7445c3bf
dithanrchy <dithanurcahya55@gmail.com>
2022-12-01 08:26:28 +0800

    update UI CustomerPurchaseViewController

--- SureBack/Views/Customer/CustomerQrCode/CustomerPurchaseViewController.swift
+++ SureBack/Views/Customer/CustomerQrCode/CustomerPurchaseViewController.swift
@@ -93,7 +93,8 @@ class CustomerPurchaseViewController: UIViewController {
         return label
     }()
 
-    let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
+    let alertSuccess = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
+    let alertWaiting = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
 
     @objc func switchStateDidChange(_ sender: UISwitch!) {
         isCoinActive = sender.isOn
@@ -101,6 +102,14 @@ class CustomerPurchaseViewController: UIViewController {
 
     @objc func makePurchase() {
         guard let coinsUsed = totalCoinValue.text, let coinsUsed = Int(coinsUsed) else { return }
+        alertWaiting.view.tintColor = UIColor.black
+        let loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(10, 5, 50, 50)) as UIActivityIndicatorView
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.gray
+        loadingIndicator.startAnimating()
+        alertWaiting.view.addSubview(loadingIndicator)
+        present(alertWaiting, animated: true, completion: nil)
+
         apiRequest.requestPurchase(merchantId: merchantId, usedCoins: coinsUsed, isRequestingForToken: useCoinSwitch.isOn) { [weak self] _ in
             guard let self = self else { return }
         }
@@ -109,7 +118,6 @@ class CustomerPurchaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         purchasePusherService = PurchasePusherSerivce(delegate: self)
-
         view.backgroundColor = .porcelain
 
         setupLayout()
@@ -118,15 +126,18 @@ class CustomerPurchaseViewController: UIViewController {
 
         let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(makePurchase))
         navigationItem.rightBarButtonItem = doneButton
-//        navigationItem.rightBarButtonItem?.tintColor = .systemBlue
         navigationItem.title = "Use Coin"
 
         guard let user = userViewModel.user else { return }
+
+        dateLabel.text = Date().dateToString()
+        usernameLabel.text = "@\(user.instagramUsername)"
 
         apiRequest.getMerchantById(id: merchantId) { [weak self] response in
             switch response {
             case let .success(merchant):
                 self?.totalCoinValue.text = String(merchant.individualCoins?.filter({ $0.coinType == "local" }).first?.outstanding ?? 0)
+                self?.merchantNameValue.text = merchant.name
             case let .failure(failure):
                 print(failure)
             }
@@ -139,8 +150,14 @@ class CustomerPurchaseViewController: UIViewController {
                 switch response {
                 case .success:
                     self.purchasePusherService?.disconnect()
-                    self.alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
-                    self.present(self.alert, animated: true, completion: nil)
+                    self.alertWaiting.dismiss(animated: true, completion: {
+                        self.alertSuccess.title = "Congratulations!"
+                        self.alertSuccess.message = "Get token and 2,000 coin used success"
+                        self.alertSuccess.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                            self.navigationController?.popViewController(animated: true)
+                        }))
+                        self.present(self.alertSuccess, animated: true, completion: nil)
+                    })
                 case let .failure(failure):
                     print(failure)
                 }
@@ -237,179 +254,3 @@ extension CustomerPurchaseViewController {
         return stackView
     }
 }
-
-// import PusherSwift
-// import UIKit
-//
-// class CustomerPurchaseViewController: UIViewController {
-//    var purchasePusherService: PurchasePusherSerivce?
-//    let merchantId: Int
-//    let userViewModel = UserViewModel.shared
-//    let apiRequest = RequestFunction()
-//    var isTokenActive = true
-//
-//    let instructionLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Please share this code to the merchant to request token."
-//        label.numberOfLines = 0
-//        label.font = UIFont.boldSystemFont(ofSize: 15)
-//        label.textAlignment = .justified
-//        label.sizeToFit()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//
-////    var qrView: CustomerPurchase = {
-////        let view = CustomerQRView()
-////        view.translatesAutoresizingMaskIntoConstraints = false
-////        return view
-////    }()
-//
-//    let balanceLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Balance"
-//        label.font = UIFont.systemFont(ofSize: 17)
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//
-//    let balanceTextField: UITextField = {
-//        let textField = UITextField()
-//        textField.borderStyle = .roundedRect
-//        textField.font = UIFont.systemFont(ofSize: 17)
-//        textField.translatesAutoresizingMaskIntoConstraints = false
-//        return textField
-//    }()
-//
-//    let getTokenLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Get Token"
-//        label.font = UIFont.systemFont(ofSize: 17)
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//
-//    let getTokenSwitch: UISwitch = {
-//        let switcher = UISwitch()
-//        switcher.isOn = true
-//        switcher.translatesAutoresizingMaskIntoConstraints = false
-//        return switcher
-//    }()
-//
-//    let buttonGenerate: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-//        button.backgroundColor = .blue
-//        button.layer.cornerRadius = 15
-//        button.setTitle("Purchase", for: .normal)
-//        return button
-//    }()
-//
-//    lazy var mainStackView: UIStackView = {
-//        let stackView = UIStackView(arrangedSubviews: [balanceLabel,
-//                                                       balanceTextField,
-//                                                       getTokenLabel,
-//                                                       getTokenSwitch,
-//                                                       buttonGenerate])
-//        stackView.axis = .vertical
-//        stackView.distribution = .fillEqually
-//        stackView.spacing = 10
-//        return stackView
-//    }()
-//
-//    let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
-//
-//    @objc func switchStateDidChange(_ sender: UISwitch!) {
-//        isTokenActive = sender.isOn
-//    }
-//
-//    @objc func makePurchase() {
-//        guard let coinsUsed = balanceTextField.text, let coinsUsed = Int(coinsUsed) else { return }
-//        apiRequest.requestPurchase(merchantId: merchantId, usedCoins: coinsUsed, isRequestingForToken: getTokenSwitch.isOn) { [weak self] _ in
-//            guard let self = self else { return }
-//        }
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        purchasePusherService = PurchasePusherSerivce(delegate: self)
-//
-//        mainStackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        view.backgroundColor = .white
-//        view.addSubview(mainStackView)
-//
-//        mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        mainStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-//        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-//
-//        getTokenSwitch.addTarget(self, action: #selector(switchStateDidChange(_:)), for: .valueChanged)
-//        getTokenSwitch.setOn(true, animated: false)
-//
-//        buttonGenerate.addTarget(self, action: #selector(makePurchase), for: .touchUpInside)
-//
-//        guard let user = userViewModel.user else { return }
-//
-//        apiRequest.getMerchantById(id: merchantId) { [weak self] response in
-//            switch response {
-//            case let .success(merchant):
-//                self?.balanceTextField.text = String(merchant.individualCoins?.filter({ $0.coinType == "local" }).first?.outstanding ?? 0)
-//            case let .failure(failure):
-//                print(failure)
-//            }
-//        }
-//
-//        purchasePusherService?.purchase(customerId: user.id, merchantId: merchantId) { [weak self] response in
-//            guard let self = self, let coins = self.balanceTextField.text, let coins = Int(coins), let _ = response.purchase else { return }
-//            self.apiRequest.requestPurchase(merchantId: self.merchantId, usedCoins: coins, isRequestingForToken: true) { [weak self] response in
-//                guard let self = self else { return }
-//                switch response {
-//                case .success:
-//                    self.purchasePusherService?.disconnect()
-//                    self.alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
-//                    self.present(self.alert, animated: true, completion: nil)
-//                case let .failure(failure):
-//                    print(failure)
-//                }
-//            }
-//        }
-//    }
-//
-//    init(merchantId: Int) {
-//        self.merchantId = merchantId
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-// }
-//
-// extension CustomerPurchaseViewController: PusherDelegate {}
-//
-// extension CustomerPurchaseViewController {
-//    func setupLayout() {
-//        setupLabel()
-//    }
-//
-//    func setupLabel() {
-//        view.addSubview(instructionLabel)
-//        instructionLabel.setTopAnchorConstraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-//        instructionLabel.setLeadingAnchorConstraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
-//        instructionLabel.setTrailingAnchorConstraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
-//    }
-//
-//    func setupDetails() {
-//        let stackView = UIStackView(arrangedSubviews: [])
-//    }
-//
-//    func setupSwitch() -> UIStackView {
-//        let stackView = UIStackView(arrangedSubviews: [getTokenLabel, getTokenSwitch])
-//        stackView.axis = .horizontal
-//        stackView.distribution = .fillProportionally
-//        stackView.alignment = .top
-//        stackView.spacing = 20
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        return stackView
-//    }
-// }
